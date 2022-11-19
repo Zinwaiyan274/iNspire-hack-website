6db1fcb3f2324c6d37f99fe1f329275a6f0a13a2
unknown <suwaiwaihnin20@gmail.com>
2022-11-19 21:36:13 +0630

    update footer icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faInstagram, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faFacebookF, faFacebookSquare, faInstagram, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import Link from 'next/link'
 
 const Footer = () => {
     return (
       <div class="footer">
-        <div className='container-fluid bg-dark text-white py-3 text-center'>
+        <div className='container-fluid footer-section text-white py-3 text-center'>
           <div className='row d-flex align-items-center justify-content-between '>
 
             <div className='col-md-4'>
@@ -22,33 +22,33 @@ const Footer = () => {
 
             <div className='col-md-4 '>
               <h3 class="fs-4">Follow Us</h3>
-              <span className='me-3 rounded-sm'>
+              <span className='me-3 rounded-sm '>
                   <Link href='https://www.facebook.com/teehtwin.org' target="_blank">
-                      <FontAwesomeIcon icon={faFacebook} style={{ fontSize:"30px" }} ></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={faFacebookF} style={{ fontSize:"25px"}} className='social-icon'></FontAwesomeIcon>
                   </Link>
               </span>
 
               <span className='me-3 rounded-sm'>
                   <Link href='https://www.youtube.com/channel/UCBraKW64CZLSyK-b49A2nBw' target="_blank">
-                      <FontAwesomeIcon icon={faYoutube} style={{ fontSize:"30px" }} ></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={faYoutube} style={{ fontSize:"25px"}} className='social-icon' ></FontAwesomeIcon>
                   </Link>
               </span>
 
               <span className='me-3 rounded-sm'>
                   <Link href='https://www.instagram.com/teehtwin' target="_blank">
-                      <FontAwesomeIcon icon={faInstagram} style={{ fontSize:"30px" }} ></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={faInstagram} style={{ fontSize:"25px"}} className='social-icon' ></FontAwesomeIcon>
                   </Link>
               </span>
             
               <span className='me-3 rounded-sm'>
                   <Link href='https://twitter.com/teehtwin' target="_blank">
-                      <FontAwesomeIcon icon={faTwitter} style={{ fontSize:"30px" }} ></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={faTwitter} style={{ fontSize:"25px"}} className='social-icon' ></FontAwesomeIcon>
                   </Link>
               </span>
             
               <span className='me-3 rounded-sm'>
                   <Link href='https://www.linkedin.com/in/tee-htwin' target="_blank">
-                      <FontAwesomeIcon icon={faLinkedin} style={{ fontSize:"30px" }} ></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={faLinkedin} style={{ fontSize:"25px"}} className='social-icon' ></FontAwesomeIcon>
                   </Link>
               </span>
 
