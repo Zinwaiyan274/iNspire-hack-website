a80fe3eed98f6fdf489b56b761eec890d94f361c
thumin <minnt5637@gmail.com>
2022-11-19 23:22:07 +0630

    update footer icon

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faInstagram, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faFacebookF, faFacebookSquare, faInstagram, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import Link from 'next/link'
 
 const Footer = () => {
     return (
       <div class="footer">
-        <div className='container-fluid bg-dark text-white py-3 text-center'>
+        <div className='container-fluid footer-section text-white py-3 text-center'>
           <div className='row d-flex align-items-center justify-content-between '>
 
             <div className='col-md-4'>
@@ -20,35 +20,35 @@ const Footer = () => {
               <p>+95-9985112943</p>
             </div>
 
-            <div className='col-md-4 mb-4'>
+            <div className='col-md-4 '>
               <h3 class="fs-4">Follow Us</h3>
-              <span className='me-3 rounded-sm'>
+              <span className='me-3 rounded-sm '>
                   <Link href='https://www.facebook.com/teehtwin.org' target="_blank">
-                      <FontAwesomeIcon icon={faFacebook} style={{ fontSize:"30px" }} ></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={faFacebookF} style={{ fontSize:"25px"}} className='social-icon'></FontAwesomeIcon>
                   </Link>
               </span>
 
               <span className='me-3 rounded-sm'>
                   <Link href='https://www.youtube.com/channel/UCBraKW64CZLSyK-b49A2nBw' target="_blank">
-                      <FontAwesomeIcon icon={faYoutube} style={{ fontSize:"30px" }} ></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={faYoutube} style={{ fontSize:"25px"}} className='social-icon' ></FontAwesomeIcon>
                   </Link>
               </span>
 
               <span className='me-3 rounded-sm'>
                   <Link href='https://www.instagram.com/teehtwin' target="_blank">
-                      <FontAwesomeIcon icon={faInstagram} style={{ fontSize:"30px" }} ></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={faInstagram} style={{ fontSize:"25px"}} className='social-icon' ></FontAwesomeIcon>
                   </Link>
               </span>
             
               <span className='me-3 rounded-sm'>
                   <Link href='https://twitter.com/teehtwin' target="_blank">
-                      <FontAwesomeIcon icon={faTwitter} style={{ fontSize:"30px" }} ></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={faTwitter} style={{ fontSize:"25px"}} className='social-icon' ></FontAwesomeIcon>
                   </Link>
               </span>
             
               <span className='me-3 rounded-sm'>
                   <Link href='https://www.linkedin.com/in/tee-htwin' target="_blank">
-                      <FontAwesomeIcon icon={faLinkedin} style={{ fontSize:"30px" }} ></FontAwesomeIcon>
+                      <FontAwesomeIcon icon={faLinkedin} style={{ fontSize:"25px"}} className='social-icon' ></FontAwesomeIcon>
                   </Link>
               </span>
 
@@ -58,7 +58,7 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className='row d-flex align-items-center justify-content-center mt-4'>
+          <div className='row d-flex align-items-center justify-content-center mt-3'>
             Copy Right @2022; Tee Htwin
           </div>
         </div>
--- pages/faq.js
+++ pages/faq.js
@@ -8,10 +8,10 @@ const faq = () => {
     <div class="faq-section">
       <div class="container-fluid faq-content">
         <div class="row mx-2 my-2">
-          <div class="col-md-3 mb-2">
+          <div class="col-md-4 col-4 mb-2">
             <div class="card" style={{ width: "25rem;",height: "17rem" }}>
               <div class="card-body">
-                <h5 class="card-title">Do you use organic milk in all your products?</h5>
+                <h5 class="card-title " style={{ fontWeight:'bold' }}>Do you use organic milk in all your products?</h5>
                 <p class="card-text">
                 We use organic milk in some products, though not all. We use it in our semi-skimmed milk in our tea, coffee, 
                 Happy Meal® milk bottles and porridge in mainland Britain and Northern Ireland.
@@ -20,10 +20,10 @@ const faq = () => {
             </div>
           </div>
 
-          <div class="col-md-3 mb-2">
-            <div class="card" style={{ width: "25rem;",height: "17rem" }}>
+          <div class="col-md-4 col-4 mb-2">
+          <div class="card" style={{ width: "25rem;",height: "17rem" }}>
               <div class="card-body">
-                <h5 class="card-title">Do you use organic milk in all your products?</h5>
+                <h5 class="card-title " style={{ fontWeight:'bold' }}>Do you use organic milk in all your products?</h5>
                 <p class="card-text">
                 We use organic milk in some products, though not all. We use it in our semi-skimmed milk in our tea, coffee, 
                 Happy Meal® milk bottles and porridge in mainland Britain and Northern Ireland.
@@ -32,10 +32,10 @@ const faq = () => {
             </div>
           </div>
 
-          <div class="col-md-3 mb-2">
-            <div class="card" style={{ width: "25rem;",height: "17rem" }}>
+          <div class="col-md-4 col-4 mb-2">
+          <div class="card" style={{ width: "25rem;",height: "17rem" }}>
               <div class="card-body">
-                <h5 class="card-title">Do you use organic milk in all your products?</h5>
+                <h5 class="card-title " style={{ fontWeight:'bold' }}>Do you use organic milk in all your products?</h5>
                 <p class="card-text">
                 We use organic milk in some products, though not all. We use it in our semi-skimmed milk in our tea, coffee, 
                 Happy Meal® milk bottles and porridge in mainland Britain and Northern Ireland.
@@ -43,25 +43,13 @@ const faq = () => {
               </div>
             </div>
           </div>
-        
-          <div class="col-md-3 mb-2">
-            <div class="card" style={{ width: "25rem;",height: "17rem" }}>
-              <div class="card-body">
-                <h5 class="card-title">Do you use organic milk in all your products?</h5>
-                <p class="card-text">
-                We use organic milk in some products, though not all. We use it in our semi-skimmed milk in our tea, coffee, 
-                Happy Meal® milk bottles and porridge in mainland Britain and Northern Ireland.
-                </p>
-              </div>
-            </div>
-          </div>
+
         </div>
-
         <div class="row mx-2 my-2">
-          <div class="col-md-3 mb-2">
-            <div class="card" style={{ width: "25rem;",height: "17rem" }}>
+          <div class="col-md-4 col-4 mb-2">
+          <div class="card" style={{ width: "25rem;",height: "17rem" }}>
               <div class="card-body">
-                <h5 class="card-title">Do you use organic milk in all your products?</h5>
+                <h5 class="card-title " style={{ fontWeight:'bold' }}>Do you use organic milk in all your products?</h5>
                 <p class="card-text">
                 We use organic milk in some products, though not all. We use it in our semi-skimmed milk in our tea, coffee, 
                 Happy Meal® milk bottles and porridge in mainland Britain and Northern Ireland.
@@ -70,10 +58,22 @@ const faq = () => {
             </div>
           </div>
 
-          <div class="col-md-3 mb-2">
-            <div class="card" style={{ width: "25rem;",height: "17rem" }}>
+          <div class="col-md-4 col-4 mb-2">
+          <div class="card" style={{ width: "25rem;",height: "17rem" }}>
               <div class="card-body">
-                <h5 class="card-title">Do you use organic milk in all your products?</h5>
+                <h5 class="card-title " style={{ fontWeight:'bold' }}>Do you use organic milk in all your products?</h5>
+                <p class="card-text">
+                We use organic milk in some products, though not all. We use it in our semi-skimmed milk in our tea, coffee, 
+                Happy Meal® milk bottles and porridge in mainland Britain and Northern Ireland.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-4 mb-2">
+          <div class="card" style={{ width: "25rem;",height: "17rem" }}>
+              <div class="card-body">
+                <h5 class="card-title " style={{ fontWeight:'bold' }}>Do you use organic milk in all your products?</h5>
                 <p class="card-text">
                 We use organic milk in some products, though not all. We use it in our semi-skimmed milk in our tea, coffee, 
                 Happy Meal® milk bottles and porridge in mainland Britain and Northern Ireland.
